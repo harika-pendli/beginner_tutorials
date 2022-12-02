57f92dfe116a136809b627d29b5d54cf9927d51f
Harika P <hpendli@umd.edu>
2022-12-01 19:01:39 -0500

    Removed override functions setup and teardown

--- test/publisherTest.cpp
+++ test/publisherTest.cpp
@@ -8,31 +8,19 @@
  */
 #include <gtest/gtest.h>
 #include <stdlib.h>
-#include <rclcpp/rclcpp.hpp>
-
-#include "../include/beginner_tutorials/MinimalPublisher.hpp"
-
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 class TestTalker : public testing::Test {
- public:
-  void SetUp() override {
-    // Setup things that should occur before every test instance should go here
-    rclcpp::init(0, nullptr);
-  }
-
-  void TearDown() override {
-    rclcpp::shutdown();
-  }
-
- protected:
-  rclcpp::Node::SharedPtr node_;
+    protected:
+     rclcpp::Node::SharedPtr node_;
 };
 
-TEST_F(TestTalker, test_num_publishers) {
+TEST_F(TestTalker, test_num_of_publishers) {
   node_ = rclcpp::Node::make_shared("test_publisher");
   auto test_pub = node_->create_publisher<std_msgs::msg::String>
                     ("topic", 10.0);
 
   auto num_pub = node_->count_publishers("topic");
-  EXPECT_EQ(1, num_pub);
+  EXPECT_EQ(1, static_cast<int>(num_pub));
 }
